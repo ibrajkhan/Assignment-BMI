6210b4b673ebfa6ebd6960f9b8728c34b50f73fb
Ibraj khan <ibraj.grd@gmail.com>
2022-01-31 18:30:31 +0530

    TodoApi Completed

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -12,8 +12,53 @@ function handelClick(e) {
   todoDiv.append(para);
   inputVal.value = "";
   count += 1;
+  console.log(para);
 }
 todoDiv.addEventListener("click", removebtn);
 function removebtn(e) {
   todoDiv.removeChild(e.target);
+}
+let selectTag = document.querySelector("#dropDown");
+selectTag.addEventListener("change", getTodoApi);
+function getTodoApi() {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      if (selectTag.value == "Completed") {
+        if (todoDiv.hasChildNodes()) {
+          while (todoDiv.firstChild) {
+            todoDiv.removeChild(todoDiv.lastChild);
+          }
+        }
+        let heading = document.createElement("h1");
+        heading.appendChild(document.createTextNode("Completed Todo List"));
+        todoDiv.appendChild(heading);
+        let todoList = data.filter((item) => item.completed);
+        // console.log(todoList);
+        todoList.forEach((item, index) => {
+          let para = document.createElement("p");
+          para.setAttribute("key", index);
+          para.appendChild(document.createTextNode(item.title));
+          todoDiv.appendChild(para);
+        });
+      } else if (selectTag.value === "Pending") {
+        if (todoDiv.hasChildNodes()) {
+          while (todoDiv.firstChild) {
+            todoDiv.removeChild(todoDiv.lastChild);
+          }
+        }
+        let heading = document.createElement("h1");
+        heading.appendChild(document.createTextNode("Pending Todo List"));
+        todoDiv.appendChild(heading);
+        let todoList = data.filter((item) => !item.completed);
+        // console.log(todoList);
+        todoList.forEach((element, index) => {
+          let para = document.createElement("p");
+          para.setAttribute("key", index);
+          para.appendChild(document.createTextNode(element.title));
+          todoDiv.appendChild(para);
+        });
+      }
+    });
 }
